815aa2c3f51c0983e29f3d95d85b528f9250bd84
Stepan <stavchig@mail.ru>
2025-02-17 17:50:15 +1000

    update v1.1

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,17 @@ from django.contrib import messages
 from .models import User, UserProfile, Course, CourseEnrollment
 from .views import send_course_approval_email
 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'russian_first_name', 'russian_last_name', 'email_verified']
+    search_fields = ['username', 'email', 'russian_first_name', 'russian_last_name']
+    list_filter = ['email_verified']
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'city', 'phone', 'birth_date']
+    search_fields = ['user__username', 'city', 'phone']
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['name', 'instructor']
@@ -10,14 +21,17 @@ class CourseAdmin(admin.ModelAdmin):
 
 @admin.register(CourseEnrollment)
 class CourseEnrollmentAdmin(admin.ModelAdmin):
-    list_display = ['user', 'course', 'status', 'created_at']
-    list_filter = ['status', 'course']
-    search_fields = ['user__username', 'user__email']
+    list_display = ['user_info', 'course', 'status', 'phone', 'city', 'address', 'created_at']
+    list_filter = ['status', 'course', 'city']
+    search_fields = ['user__username', 'user__email', 'user__russian_first_name', 'user__russian_last_name', 'phone', 'city']
     list_editable = ['status']
+    
+    def user_info(self, obj):
+        return f"{obj.user.russian_last_name} {obj.user.russian_first_name} ({obj.user.email})"
+    user_info.short_description = 'Пользователь'
     
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
-        # Если статус меняется на "approved"
         if 'status' in form.changed_data and obj.status == 'approved':
             if send_course_approval_email(obj):
                 messages.success(request, f'Письмо успешно отправлено пользователю {obj.user.email}')
